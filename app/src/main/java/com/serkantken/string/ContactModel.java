d812084d12577a1f50ad04826f01817771b03c43
Serkan Tiken <serkantkn5@gmail.com>
2022-12-02 12:00:56 +0300

    Her kişi bilgisi ayrı satırda

---
+++ app/src/main/java/com/serkantken/string/ContactModel.java
@@ -0,0 +1,43 @@
+package com.serkantken.string;
+
+public class ContactModel
+{
+    String name, username, email, id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return name + ' ' + username + ' ' + email;
+    }
+}
--- app/src/main/java/com/serkantken/string/MainActivity.java
+++ app/src/main/java/com/serkantken/string/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,17 +25,33 @@ public class MainActivity extends AppCompatActivity {
                     "name=Robert Nick II, username=rnickTemp34, id=23432, email=rnick@gmail.com; " +
                     "name=Susan Vee, username=sv55, id=443432, email=susanv123@me.com;";
 
-        String[] split = yazi.split(",");
-        String result = "";
+        String[] split1 = yazi.split(";");
+        ArrayList<ContactModel> contacts = new ArrayList<>();
 
-        for (String string : split)
+        for (String value : split1)
         {
-            if (!string.contains("id="))
+            String[] temp = value.split(",");
+            ContactModel contact = new ContactModel();
+            for (String s : temp)
             {
-                result = result + string;
+                if (s.contains("user"))
+                {
+                    contact.username = s;
+                }
+                else if (s.contains("email"))
+                {
+                    contact.email = s;
+                }
+                else if (!s.contains("id"))
+                {
+                    contact.name = s;
+                }
             }
+            contacts.add(contact);
         }
-
-        Log.i("sonuc", result);
+        for (ContactModel contactModel : contacts)
+        {
+            Log.i("sonuc", contactModel.name.trim() + " / " + contactModel.username.trim() + " / " + contactModel.email.trim());
+        }
     }
 }
